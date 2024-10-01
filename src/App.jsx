5083058c90da75c6e5c8ca04f0fca1a75cddcc84
Anshul-Raghuwanshi <111377828+Anshul-Raghuwanshi@users.noreply.github.com>
2024-10-01 22:30:55 +0530

    fisrt commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,26 +2,28 @@ import React from 'react';
 
 function App() {
   return (
-    <div className="min-h-screen flex">
+    <div className="min-h-screen flex flex-col md:flex-row">
       {/* Left Section */}
-      <div className="bg-black w-1/3 flex flex-col justify-between p-10 text-white relative z-10">
+      <div className="bg-black w-full md:w-1/3 flex flex-col justify-between p-10 text-white relative z-10">
         <div className="space-y-5">
           <h1 className="text-3xl font-bold">PORTFOLIO</h1>
         </div>
         {/* Adjusted to move content more towards the left and higher from the bottom */}
-        <div className="flex flex-col items-start absolute bottom-40 left-40 space-y-5">
+        <div className="flex flex-col items-start absolute bottom-40 left-10 md:left-40 space-y-5">
           <div className="flex flex-col">
-            <h2 className="text-5xl font-bold">My Name Is</h2>
-            <h2 className="text-5xl font-bold text-yellow-400 whitespace-nowrap">ANSHUL RAGHUWANSHI</h2>
+            <h2 className="text-3xl md:text-5xl font-bold">My Name Is</h2>
+            <h2 className="text-3xl md:text-5xl font-bold text-yellow-400 whitespace-nowrap">
+              ANSHUL RAGHUWANSHI
+            </h2>
           </div>
-          <button className="bg-yellow-400 text-black px-6 py-3 mt-5 text-xl hover:bg-yellow-300 transition duration-300 whitespace-nowrap">
+          <button className="bg-yellow-400 text-black px-4 md:px-6 py-2 md:py-3 mt-5 text-lg md:text-xl hover:bg-yellow-300 transition duration-300 whitespace-nowrap">
             I'M A UI DEVELOPER
           </button>
         </div>
       </div>
 
       {/* Right Section */}
-      <div className="relative w-2/3">
+      <div className="relative w-full md:w-2/3">
         {/* Image as Background */}
         <div className="absolute inset-0">
           <img
@@ -32,7 +34,7 @@ function App() {
         </div>
         
         {/* Navbar */}
-        <header className="absolute top-0 right-0 left-0 flex justify-center space-x-8 text-white p-10 bg-transparent z-20">
+        <header className="absolute top-0 right-0 left-0 flex justify-center space-x-4 md:space-x-8 text-white p-5 md:p-10 bg-transparent z-20">
           <a href="#" className="hover:text-yellow-400 hover:border-b-2 hover:border-yellow-400 pb-1 transition duration-300">Home</a>
           <a href="#" className="hover:text-yellow-400 hover:border-b-2 hover:border-yellow-400 pb-1 transition duration-300">About</a>
           <a href="#" className="hover:text-yellow-400 hover:border-b-2 hover:border-yellow-400 pb-1 transition duration-300">Works</a>
